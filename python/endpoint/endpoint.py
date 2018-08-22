d3749ae22477653a8f9ce708388764c701d2eaaa
Dan Serban <dserban01@yahoo.com>
2018-08-22 23:47:00 +0200

    Update endpoint.py

--- python/endpoint/endpoint.py
+++ python/endpoint/endpoint.py
@@ -1,9 +1,33 @@
 from os import getenv
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
 
 from flask import (render_template as rt,
                    Flask, request, redirect, url_for, session, jsonify)
 
 from gevent.pywsgi import WSGIServer
+
+class Cassandra:
+    def __init__(self):
+        self.MORPHL_SERVER_IP_ADDRESS = getenv('MORPHL_SERVER_IP_ADDRESS')
+        self.MORPHL_CASSANDRA_USERNAME = getenv('MORPHL_CASSANDRA_USERNAME')
+        self.MORPHL_CASSANDRA_PASSWORD = getenv('MORPHL_CASSANDRA_PASSWORD')
+        self.MORPHL_CASSANDRA_KEYSPACE = getenv('MORPHL_CASSANDRA_KEYSPACE')
+
+        self.auth_provider = PlainTextAuthProvider(
+            username=self.MORPHL_CASSANDRA_USERNAME, password=self.MORPHL_CASSANDRA_PASSWORD)
+        self.cluster = Cluster(
+            [self.MORPHL_SERVER_IP_ADDRESS], auth_provider=self.auth_provider)
+        self.session = self.cluster.connect(self.MORPHL_CASSANDRA_KEYSPACE)
+        self.session.default_fetch_size = 1
+
+        self.prepare_statement()
+
+    def prepare_statement(self):
+        pass
+
+    def retrieve_prediction(self, client_id):
+        pass
 
 app = Flask(__name__)
 
