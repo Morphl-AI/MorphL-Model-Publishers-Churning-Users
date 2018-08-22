0893cf3aff58a4d5a8d5b375b07280d2098130d3
Dan Serban <dserban01@yahoo.com>
2018-08-22 20:49:49 +0200

    Create endpoint.py

---
+++ python/endpoint/endpoint.py
@@ -0,0 +1,20 @@
+from os import getenv
+from uuid import uuid1
+
+from flask import (render_template as rt,
+                   Flask, request, redirect, url_for, session, jsonify)
+
+from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.config['SECRET_KEY'] = str(uuid1())
+    if getenv('DEBUG'):
+        app.config['DEBUG'] = True
+        flask_port = 5858
+        app.run(host='0.0.0.0', port=flask_port)
+    else:
+        app.config['DEBUG'] = False
+        flask_port = 6868
+        WSGIServer(('', flask_port), app).serve_forever()
