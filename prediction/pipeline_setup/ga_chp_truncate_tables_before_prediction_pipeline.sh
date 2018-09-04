887698e11fde05ba46fe6381c7b5ea3f31cb8bb8
Dan Serban <dserban01@yahoo.com>
2018-09-04 20:24:05 +0200

    Update ga_chp_truncate_tables_before_prediction_pipeline.sh

--- prediction/pipeline_setup/ga_chp_truncate_tables_before_prediction_pipeline.sh
+++ prediction/pipeline_setup/ga_chp_truncate_tables_before_prediction_pipeline.sh
@@ -1,7 +1,7 @@
 cqlsh ${MORPHL_SERVER_IP_ADDRESS} -u morphl -p ${MORPHL_CASSANDRA_PASSWORD} \
   -f /opt/ga_chp/prediction/pipeline_setup/ga_chp_truncate_tables_before_prediction_pipeline.cql
 
-HDFS_PORT = 9000
+HDFS_PORT=9000
 
 HDFS_DIR_PREPROC=hdfs://${MORPHL_SERVER_IP_ADDRESS}:${HDFS_PORT}/${DAY_AS_STR}_${UNIQUE_HASH}_ga_chp_preproc_prediction
 
