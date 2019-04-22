8346a890b6fb2b114c34b620cbc25e8c745d1df6
abarbulescu <alexandru@appticles.com>
2019-04-22 18:42:41 +0300

    Update batch inference

--- prediction/batch_inference/ga_chp_batch_inference.py
+++ prediction/batch_inference/ga_chp_batch_inference.py
@@ -1,107 +1,148 @@
 from os import getenv
+
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
-from distributed import Client
+
 from keras.models import load_model
 
-import dask.dataframe as dd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
 
-DAY_AS_STR = getenv('DAY_AS_STR')
+import numpy as np
+
+MORPHL_SERVER_IP_ADDRESS = getenv('MORPHL_SERVER_IP_ADDRESS')
+MORPHL_CASSANDRA_USERNAME = getenv('MORPHL_CASSANDRA_USERNAME')
+MORPHL_CASSANDRA_PASSWORD = getenv('MORPHL_CASSANDRA_PASSWORD')
+MORPHL_CASSANDRA_KEYSPACE = getenv('MORPHL_CASSANDRA_KEYSPACE')
+
+DAY_AS_STR = getenv('DAY_AS_STRING')
 MODEL_DAY_AS_STR = getenv('MODEL_DAY_AS_STR')
 UNIQUE_HASH = getenv('UNIQUE_HASH')
 
 MORPHL_SERVER_IP_ADDRESS = getenv('MORPHL_SERVER_IP_ADDRESS')
+MASTER_URL = 'local[*]'
+APPLICATION_NAME = 'batch-inference'
 
 HDFS_PORT = 9000
 HDFS_DIR_INPUT = f'hdfs://{MORPHL_SERVER_IP_ADDRESS}:{HDFS_PORT}/{MODEL_DAY_AS_STR}_{UNIQUE_HASH}_ga_chp_scaled_features_prediction'
 
-
-class Cassandra:
-    def __init__(self):
-        self.MORPHL_SERVER_IP_ADDRESS = getenv('MORPHL_SERVER_IP_ADDRESS')
-        self.MORPHL_CASSANDRA_USERNAME = getenv('MORPHL_CASSANDRA_USERNAME')
-        self.MORPHL_CASSANDRA_PASSWORD = getenv('MORPHL_CASSANDRA_PASSWORD')
-        self.MORPHL_CASSANDRA_KEYSPACE = getenv('MORPHL_CASSANDRA_KEYSPACE')
-
-        self.prep_stmt = {}
-
-        template_for_prediction = 'INSERT INTO ga_chp_predictions (client_id,prediction) VALUES (?,?)'
-        template_for_predictions_by_date = 'INSERT INTO ga_chp_predictions_by_prediction_date (prediction_date, client_id, prediction) VALUES (?,?,?)'
-        template_for_predictions_statistics = 'UPDATE ga_chp_predictions_statistics SET loyal=loyal+?, neutral=neutral+?, churning=churning+?, lost=lost+? WHERE prediction_date=?'
-
-        self.CASS_REQ_TIMEOUT = 3600.0
-
-        self.auth_provider = PlainTextAuthProvider(
-            username=self.MORPHL_CASSANDRA_USERNAME,
-            password=self.MORPHL_CASSANDRA_PASSWORD)
-        self.cluster = Cluster(
-            [self.MORPHL_SERVER_IP_ADDRESS], auth_provider=self.auth_provider)
-        self.session = self.cluster.connect(self.MORPHL_CASSANDRA_KEYSPACE)
-
-        self.prep_stmt['prediction'] = self.session.prepare(
-            template_for_prediction)
-        self.prep_stmt['predictions_by_date'] = self.session.prepare(
-            template_for_predictions_by_date)
-        self.prep_stmt['predictions_statistics'] = self.session.prepare(
-            template_for_predictions_statistics)
-
-    def update_predictions_statistics(self, series_obj):
-
-        loyal = series_obj[series_obj <= 0.4].count().compute()
-
-        neutral = series_obj[(series_obj > 0.4) & (
-            series_obj <= 0.6)].count().compute()
-
-        churning = series_obj[(series_obj > 0.6) & (
-            series_obj <= 0.9)].count().compute()
-
-        lost = series_obj[(series_obj > 0.9) & (
-            series_obj <= 1)].count().compute()
-
-        bind_list = [loyal, neutral, churning, lost, DAY_AS_STR]
-
-        self.session.execute(
-            self.prep_stmt['predictions_statistics'], bind_list, timeout=self.CASS_REQ_TIMEOUT)
-
-    def save_prediction_by_date(self, client_id, prediction):
-        bind_list = [DAY_AS_STR, client_id, prediction]
-
-        self.session.execute(
-            self.prep_stmt['predictions_by_date'], bind_list, timeout=self.CASS_REQ_TIMEOUT)
-
-    def save_prediction(self, client_id, prediction):
-        bind_list = [client_id, prediction]
-
-        self.session.execute(self.prep_stmt['prediction'], bind_list,
-                             timeout=self.CASS_REQ_TIMEOUT)
+CASS_REQ_TIMEOUT = 3600.0
 
 
-def batch_inference_on_partition(partition_df):
+def update_predictions_statistics(predictions_df):
+
+    auth_provider = PlainTextAuthProvider(
+        username=MORPHL_CASSANDRA_USERNAME,
+        password=MORPHL_CASSANDRA_PASSWORD
+    )
+
+    cluster = Cluster(
+        [MORPHL_SERVER_IP_ADDRESS], auth_provider=auth_provider)
+
+    spark_session_cass = cluster.connect(MORPHL_CASSANDRA_KEYSPACE)
+
+    prep_stmt_predictions_statistics = spark_session_cass.prepare(
+        'UPDATE ga_chp_predictions_statistics SET loyal=loyal+?, neutral=neutral+?, churning=churning+?, lost=lost+? WHERE prediction_date=?')
+
+    loyal = predictions_df.filter('prediction < 0.4').count()
+    neutral = predictions_df.filter(
+        '(prediction > 0.4) and (prediction <= 0.6)').count()
+    churning = predictions_df.filter(
+        '(prediction > 0.6) and (prediction <= 0.9)').count()
+    lost = predictions_df.filter(
+        '(prediction > 0.9) and (prediction <= 1.0)').count()
+
+    bind_list = [loyal, neutral, churning, lost, DAY_AS_STR]
+
+    spark_session_cass.execute(
+        prep_stmt_predictions_statistics, bind_list, timeout=CASS_REQ_TIMEOUT)
+
+
+def batch_inference(partition):
     churn_model_file = f'/opt/models/{MODEL_DAY_AS_STR}_{UNIQUE_HASH}_ga_chp_churn_model.h5'
     churn_model = load_model(churn_model_file)
-    prediction = churn_model.predict(
-        partition_df.drop(['client_id'], axis=1))[0][0]
-    return prediction
+
+    result = []
+
+    for row in partition:
+        array_to_predict = np.array([[row.pageviews,
+                                      row.unique_pageviews,
+                                      row.hits,
+                                      row.u_sessions,
+                                      row.entrances,
+                                      row.bounces,
+                                      row.exits,
+                                      row.session_count,
+                                      row.is_desktop,
+                                      row.is_mobile,
+                                      row.is_tablet,
+                                      row.session_duration,
+                                      row.time_on_page
+                                      ]])
+
+        result.append((row.client_id, ) +
+                      tuple(churn_model.predict(array_to_predict).tolist()[0]))
+
+    return iter(result)
 
 
-def persist_partition(partition_df):
-    def persist_one_prediction(series_obj):
-        cassandra.save_prediction_by_date(
-            series_obj.client_id, series_obj.prediction)
-        cassandra.save_prediction(series_obj.client_id, series_obj.prediction)
-    cassandra = Cassandra()
-    partition_df.apply(persist_one_prediction, axis=1)
-    return 0
+def main():
+    spark_session = (
+        SparkSession.builder
+        .appName(APPLICATION_NAME)
+        .master(MASTER_URL)
+        .config('spark.cassandra.connection.host', MORPHL_SERVER_IP_ADDRESS)
+        .config('spark.cassandra.auth.username', MORPHL_CASSANDRA_USERNAME)
+        .config('spark.cassandra.auth.password', MORPHL_CASSANDRA_PASSWORD)
+        .config('spark.sql.shuffle.partitions', 16)
+        .getOrCreate()
+    )
+
+    log4j = spark_session.sparkContext._jvm.org.apache.log4j
+    log4j.LogManager.getRootLogger().setLevel(log4j.Level.ERROR)
+
+    df = (spark_session.read.parquet(HDFS_DIR_INPUT))
+
+    predictions_df = (df
+                      .rdd
+                      .mapPartitions(batch_inference)
+                      .repartition(32)
+                      .toDF()
+                      .withColumnRenamed('_1', 'client_id')
+                      .withColumnRenamed('_2', 'prediction')
+                      )
+
+    save_options_ga_chp_predictions = {
+        'keyspace': MORPHL_CASSANDRA_KEYSPACE,
+        'table': 'ga_chp_predictions'
+    }
+
+    save_options_ga_chp_predictions_by_date = {
+        'keyspace': MORPHL_CASSANDRA_KEYSPACE,
+        'table': 'ga_chp_predictions_by_prediction_date'
+    }
+
+    (predictions_df
+     .write
+     .format('org.apache.spark.sql.cassandra')
+     .mode('append')
+     .options(**save_options_ga_chp_predictions)
+     .save()
+     )
+
+    predictions_by_date_df = predictions_df.withColumn(
+        'prediction_date', lit(DAY_AS_STR))
+
+    (predictions_by_date_df
+     .write
+     .format('org.apache.spark.sql.cassandra')
+     .mode('append')
+     .options(**save_options_ga_chp_predictions_by_date)
+     .save()
+     )
+
+    update_predictions_statistics(predictions_df)
 
 
 if __name__ == '__main__':
-    client = Client()
-    cassandra = Cassandra()
-    dask_df = client.persist(dd.read_parquet(HDFS_DIR_INPUT))
-    dask_df.client_id.count().compute()
-    dask_df['prediction'] = dask_df.map_partitions(
-        batch_inference_on_partition, meta=('prediction', float))
-    cassandra.update_predictions_statistics(dask_df['prediction'])
-    dask_df['token'] = dask_df.map_partitions(
-        persist_partition, meta=('token', int))
-    dask_df.token.compute()
+    main()
